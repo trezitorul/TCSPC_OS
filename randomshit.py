5af0d590c573f62b3bcf0ecc93b055c5fe16b92c
longnguyen0810 <nguye669@purdue.edu>
2023-02-26 13:18:19 -0500

    Update randomshit.py

--- randomshit.py
+++ randomshit.py
@@ -1,6 +1,14 @@
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
+import clr
+clr.AddReference('System')
+from System import Decimal
+
+A = Decimal(5)
+print(A)
+B = Decimal.ToDouble(A)
+print(B)
 tArray = []
 yArray = []
 for t in range(100):
