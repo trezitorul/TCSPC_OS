fbf2248228f1de8cf570a6723fc2fa23e1eaf94b
longnguyen0810 <nguye669@purdue.edu>
2023-02-26 15:55:57 -0500

    Update randomshit.py

--- randomshit.py
+++ randomshit.py
@@ -1,19 +1,28 @@
+import os
+import time
+import logging
+import sys
+import clr
+import matplotlib.pyplot as plt
 from scipy import signal
 import numpy as np
-import matplotlib.pyplot as plt
-import clr
-clr.AddReference('System')
-from System import Decimal
+from ctypes import *
+#from System import Decimal
+import decimal
+sys.path.append(r"C:\\Program Files\\Thorlabs\\Kinesis")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.GenericMotorCLI.dll")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.Benchtop.PiezoCLI.dll")
+clr.AddReference("System.Collections")
+clr.AddReference("System.Linq")
 
-A = Decimal(5)
-print(A)
-B = Decimal.ToDouble(A)
-print(B)
-tArray = []
-yArray = []
-for t in range(100):
-    tArray.append(t)
-    y = signal.square(2*np.pi*2*t)
-    yArray.append(y)
-plt.plot(tArray, yArray)
-plt.show()
+from Thorlabs.MotionControl.DeviceManagerCLI import DeviceManagerCLI 
+from Thorlabs.MotionControl.DeviceManagerCLI import DeviceNotReadyException 
+import Thorlabs.MotionControl.GenericPiezoCLI.Piezo as Piezo 
+from Thorlabs.MotionControl.Benchtop.PiezoCLI import BenchtopPiezo 
+
+DeviceManagerCLI.BuildDeviceList()
+deviceID = "71201654"
+device =  BenchtopPiezo.CreateBenchtopPiezo(str(deviceID))
+device.Connect(deviceID)
+print(device)
