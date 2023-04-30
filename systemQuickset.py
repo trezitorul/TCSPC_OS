a55a78d12bd7e6b83c335e77c9e3c37ddd5dffb8
longnguyen0810 <nguye669@purdue.edu>
2023-04-30 15:48:25 -0400

    Create systemQuickset.py

---
+++ systemQuickset.py
@@ -0,0 +1,18 @@
+import sys
+import  time
+sys.path.append("C:\\Users\\Ozymandias\\TCSPC_project\\LAC")
+sys.path.append("C:\\Users\\Ozymandias\\TCSPC_project\\MFF101FlipperMirror")
+from LAC import LAC
+from MFF101FlipperMirror import MFF101FlipperMirror
+#cameraFlipperID = "37005411"
+#cameraFlipper = MFF101FlipperMirror(cameraFlipperID)
+#mirror.HomeMirror()
+#beamBlockFlipperID="37005466"
+#beamBlockFlipper = MFF101FlipperMirror(beamBlockFlipperID)
+
+#cameraFlipper.SetMode("off")
+#beamBlockFlipper.SetMode("off")
+#time.sleep(1)
+lac = LAC()
+time.sleep(1)
+lac.set_position(int(0.7*1023))
